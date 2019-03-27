e7fa7c60a7537901519bf2419cd45d364689246c
OROSEV <zhenka-oros@mail.ru>
2019-03-27 13:38:05 +0200

    second

--- app/admin/abouthotels.rb
+++ app/admin/abouthotels.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Abouthotel do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :list, :of, :attributes, :on, :model
+# permit_params :list, :of, :attributes, :on, :model
 #
 # or
 #
@@ -11,5 +11,5 @@ ActiveAdmin.register Abouthotel do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+permit_params :title, :address, :rooms, :photo 
 end
